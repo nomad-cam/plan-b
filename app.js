849c3f8f1059b97aacc2041b96b51fee58d9fcd3
RobbieClarken <rob.clarken@gmail.com>
2012-08-31 16:07:58 +1000

    Checks tickets

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var tatts = require('./lib/tatts');
+var tatts = require('./lib/tatts')
+  , ourNumbers = require('./numbers');
 
 tatts.latest(function(err, mainNumbers, supplementaryNumbers) {
   if(!err) {
     console.log(mainNumbers);
     console.log(supplementaryNumbers);
+    var results = tatts.checkTicket(ourNumbers, mainNumbers, supplementaryNumbers);
+    console.log(JSON.stringify(results));
   } else {
     console.log('Err: ', err);
   }
