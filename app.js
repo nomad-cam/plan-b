87b48bedc423875331bb9c6f67b02722261659fc
RobbieClarken <rob.clarken@gmail.com>
2012-09-01 00:16:45 +1000

    Web page shows winning numbers

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var tatts = require('./lib/tatts')
-  , ourNumbers = require('./numbers');
+  , ourNumbers = require('./numbers')
+  , http = require('http')
+  , jade = require('jade')
+  , fs = require('fs');
 
-tatts.latest(function(err, mainNumbers, supplementaryNumbers) {
-  if(!err) {
-    console.log(mainNumbers);
-    console.log(supplementaryNumbers);
-    var results = tatts.checkTicket(ourNumbers, mainNumbers, supplementaryNumbers);
-    console.log(JSON.stringify(results));
+var index = jade.compile(fs.readFileSync('index.jade', 'utf8'));
+
+http.createServer(function(req, res) {
+  if(req.url === '/style.css') {
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      fs.readFile('style.css', 'utf8', function(err, data) {
+        res.end(data+'\n');
+      });
   } else {
-    console.log('Err: ', err);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    tatts.latest(function(err, mainNumbers, supplementaryNumbers) {
+      if(!err) {
+        var results = tatts.checkTicket(ourNumbers, mainNumbers, supplementaryNumbers);
+        res.end(index({games: results})+'\n');
+      } else {
+        console.log('Err: ', err);
+      }
+    });
   }
-});
+}).listen(9010, '127.0.0.1');
