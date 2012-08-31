f91a113ef82858a4b3b35f980897560ebdb27ca7
RobbieClarken <rob.clarken@gmail.com>
2012-08-31 15:06:50 +1000

    Error handling

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var tatts = require('./lib/tatts');
 
-tatts.latest();
+tatts.latest(function(err, mainNumbers, supplementaryNumbers) {
+  if(!err) {
+    console.log(mainNumbers);
+    console.log(supplementaryNumbers);
+  } else {
+    console.log('Err: ', err);
+  }
+});
