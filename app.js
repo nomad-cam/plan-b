896a3a1982bd712e28bbd2398228a43ace1991a1
RobbieClarken <robbie.clarken@gmail.com>
2017-04-01 14:33:30 +1100

    Fix matching numbers div

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ http.createServer(function(req, res) {
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     tatts.latest(function(err, mainNumbers, supplementaryNumbers, date, coin) {
-      if(!err) {        
-        var results = tatts.checkTicket(ourNumbers, mainNumbers, supplementaryNumbers, coin);        
+      if(!err) {
+        var results = tatts.checkTicket(ourNumbers, mainNumbers, supplementaryNumbers, coin);
         res.end(index({date: date, games: results })+'\n');
       } else {
         console.log('Err: ', err);
